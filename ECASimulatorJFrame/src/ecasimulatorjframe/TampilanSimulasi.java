a5379c45a143b96646fb87f21797a88a89ba2af4
Vanessa <7314010@student.unpar.ac.id>
2018-10-29 23:25:20 +0700

    codingan skripsi (11.1)

--- ECASimulatorJFrame/src/ecasimulatorjframe/TampilanSimulasi.java
+++ ECASimulatorJFrame/src/ecasimulatorjframe/TampilanSimulasi.java
@@ -17,9 +17,10 @@ public class TampilanSimulasi extends javax.swing.JFrame {
      * Creates new form TampilanSimulasi
      */
     CA ca;
+
     public TampilanSimulasi(CA ca) {
         initComponents();
-        this.ca=ca;
+        this.ca = ca;
     }
 
     /**
@@ -225,9 +226,9 @@ public class TampilanSimulasi extends javax.swing.JFrame {
 
     private void simulateButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_simulateButtonMouseClicked
         boolean checker = true;
-        double a=0.0;
-        double b=0.0;
-        double c=0.0;
+        double a = 0.0;
+        double b = 0.0;
+        double c = 0.0;
         if (nilaiA.getText().equals("")) {
             InputDataHandler.inputDataSimulasi("nilaiA", null);
             checker = false;
@@ -247,29 +248,30 @@ public class TampilanSimulasi extends javax.swing.JFrame {
             if (!nilaiA.getText().equals("")) {
                 InputDataHandler.inputDataSimulasi("nilaiA", nilaiA.getText());
                 a = Double.parseDouble(nilaiA.getText());
-                checker = true;
-            } else if (!nilaiB.getText().equals("")) {
-                InputDataHandler.inputDataSimulasi("nilaiB", nilaiB.getText());
-                b = Double.parseDouble(nilaiB.getText());
-                checker = true;
-            } else if (!nilaiC.getText().equals("")) {
-                InputDataHandler.inputDataSimulasi("nilaiC", nilaiC.getText());
-                c = Double.parseDouble(nilaiC.getText());
-                checker = true;
-            } else if (!nilaiPeriode.getText().equals("")) {
-                InputDataHandler.inputDataSimulasi("periode", nilaiPeriode.getText());
-                checker = true;
-            } else {
-                InputDataHandler.inputDataSimulasi("threshold", nilaiThreshold.getText());
-                checker = true;
+                if (!nilaiB.getText().equals("")) {
+                    InputDataHandler.inputDataSimulasi("nilaiB", nilaiB.getText());
+                    b = Double.parseDouble(nilaiB.getText());
+                    if (!nilaiC.getText().equals("")) {
+                        InputDataHandler.inputDataSimulasi("nilaiC", nilaiC.getText());
+                        c = Double.parseDouble(nilaiC.getText());
+                        if (!nilaiPeriode.getText().equals("")) {
+                            InputDataHandler.inputDataSimulasi("periode", nilaiPeriode.getText());
+                            System.out.println(nilaiPeriode.getText());
+                            if (!nilaiThreshold.getText().equals("")) {
+                                InputDataHandler.inputDataSimulasi("threshold", nilaiThreshold.getText());
+                                System.out.println(nilaiThreshold.getText());
+                            }
+                        }
+                    }
+                }
             }
         }
-        
-        if (a+b+c!=1.0) {
+
+        if (a + b + c != 1.0) {
             JOptionPane.showMessageDialog(null, "The sum of a,b and c's value must 1!");
             checker = false;
         }
-        
+
         if (checker == true) {
             this.hide();
             TampilanDataWirausaha ks = new TampilanDataWirausaha();
@@ -277,72 +279,71 @@ public class TampilanSimulasi extends javax.swing.JFrame {
         } else {
             JOptionPane.showMessageDialog(null, "You must fill the text field first!");
         }
-        
-        double[] composition = new double[] {a,b,c};
-        
-        double[] POAf = new double [] {8.6,17.7,28.4,29.5,15.8}; // female
-        double[] POAm = new double [] {8.3,14.5,26.7,36.2,14.3}; // male
-        
+
+        double[] composition = new double[]{a, b, c};
+
+        double[] POAf = new double[]{8.6, 17.7, 28.4, 29.5, 15.8}; // female
+        double[] POAm = new double[]{8.3, 14.5, 26.7, 36.2, 14.3}; // male
+
         // Perceived Opportunities Education
-        double[] POEf = new double [] {0.1,7.4,49.8,23.4,17.4,1.8};
-        double[] POEm = new double [] {0.3,12.6,54.7,19.9,11.8,0.7};
-        
+        double[] POEf = new double[]{0.1, 7.4, 49.8, 23.4, 17.4, 1.8};
+        double[] POEm = new double[]{0.3, 12.6, 54.7, 19.9, 11.8, 0.7};
+
         // Perceived Opportunities Location
-        double[] POLf = new double [] {2.4,0.6,1.0,1.1,2.5,9.5,6.1,9.6,46.3,1.7,3.5,1.4,2.8,4.8,6.4,0.3};
-        double[] POLm = new double [] {2.0,0.5,1.1,0.9,2.7,8.4,6.3,11.1,47.6,2.1,3.8,1.9,2.3,4.5,4.4,0.5};
-        
+        double[] POLf = new double[]{2.4, 0.6, 1.0, 1.1, 2.5, 9.5, 6.1, 9.6, 46.3, 1.7, 3.5, 1.4, 2.8, 4.8, 6.4, 0.3};
+        double[] POLm = new double[]{2.0, 0.5, 1.1, 0.9, 2.7, 8.4, 6.3, 11.1, 47.6, 2.1, 3.8, 1.9, 2.3, 4.5, 4.4, 0.5};
+
         // Perceived Opportunities Income
-        double[] POIf = new double[] {0.5,0,0.3,1.5,2.8,10.8,41.5,42.7};
-        double[] POIm = new double[] {0.2,0.5,0.3,0.7,3.4,11.0,41.7,42.1};
-        
+        double[] POIf = new double[]{0.5, 0, 0.3, 1.5, 2.8, 10.8, 41.5, 42.7};
+        double[] POIm = new double[]{0.2, 0.5, 0.3, 0.7, 3.4, 11.0, 41.7, 42.1};
+
         // Perceived Capabilities Age
-        double[] PCAf = new double[] {8.9,16.1,28.2,31.6,15.1};
-        double[] PCAm = new double[] {8.5,17.3,26.1,33.4,14.7};
-        
+        double[] PCAf = new double[]{8.9, 16.1, 28.2, 31.6, 15.1};
+        double[] PCAm = new double[]{8.5, 17.3, 26.1, 33.4, 14.7};
+
         // Perceived Capabilities Education
-        double[] PCEf = new double[] {0.4,8.2,51.5,22.8,15.4,1.7};
-        double[] PCEm = new double[] {0.5,12.0,56.9,17.4,12.4,0.9};
-        
+        double[] PCEf = new double[]{0.4, 8.2, 51.5, 22.8, 15.4, 1.7};
+        double[] PCEm = new double[]{0.5, 12.0, 56.9, 17.4, 12.4, 0.9};
+
         // Perceived Capabilities Income
-        double[] PCIf = new double[] {0.4,0.2,0.2,1.4,3.1,10.2,43.0,41.4};
-        double[] PCIm = new double[] {0.2,0.2,0.3,0.8,3.1,10.5,42.1,42.9};
-        
+        double[] PCIf = new double[]{0.4, 0.2, 0.2, 1.4, 3.1, 10.2, 43.0, 41.4};
+        double[] PCIm = new double[]{0.2, 0.2, 0.3, 0.8, 3.1, 10.5, 42.1, 42.9};
+
         // Perceived Capabilities Location
-        double[] PCLf = new double[] {4.4,0.8,1.0,1.2,3.7,9.0,6.9,10.8,41.1,1.6,3.2,2.0,2.4,3.9,7.5,0.4};
-        double[] PCLm = new double[] {4.3,0.7,0.9,1.2,3.5,8.7,7.2,11.5,41.1,1.8,3.8,3.0,2.6,3.4,5.4,0.7};
-        
+        double[] PCLf = new double[]{4.4, 0.8, 1.0, 1.2, 3.7, 9.0, 6.9, 10.8, 41.1, 1.6, 3.2, 2.0, 2.4, 3.9, 7.5, 0.4};
+        double[] PCLm = new double[]{4.3, 0.7, 0.9, 1.2, 3.5, 8.7, 7.2, 11.5, 41.1, 1.8, 3.8, 3.0, 2.6, 3.4, 5.4, 0.7};
+
         // Role Model Age
-        double[] RMAf = new double[] {7.5,17.6,26.8,31.0,17.1};
-        double[] RMAm = new double[] {9.1,16.9,25.3,34.4,14.3};
-        
+        double[] RMAf = new double[]{7.5, 17.6, 26.8, 31.0, 17.1};
+        double[] RMAm = new double[]{9.1, 16.9, 25.3, 34.4, 14.3};
+
         // Role Model Income
-        double[] RMIf = new double[] {0.4,0.1,0.4,1.6,3.0,9.7,41.8,43.1};
-        double[] RMIm = new double[] {0.2,0.5,0.2,0.7,3.0,10.4,42.1,42.9};
-        
+        double[] RMIf = new double[]{0.4, 0.1, 0.4, 1.6, 3.0, 9.7, 41.8, 43.1};
+        double[] RMIm = new double[]{0.2, 0.5, 0.2, 0.7, 3.0, 10.4, 42.1, 42.9};
+
         // Faktor Publik
-        double[] pfs = new double[] {2.53, 3.92, 3.29, 3.45};
+        double[] pfs = new double[]{2.53, 3.92, 3.29, 3.45};
         double[] pfw = InputDataHandler.getDataEksternal();
-        
+
         double[] nw = InputDataHandler.getBobot();
         int[] nr = InputDataHandler.getRelation();
-        
-        
+
         ca.pub.setFactors(pfs);
         ca.pub.setWeights(pfw);
-        
+
         ca.N.setWeight(nw);
         ca.N.setRelation(nr);
-        
+
         int maxIter = 100;
         for (int i = 0; i < maxIter; i++) {
             ca.NeighborhoodDefinition();
-           // int periode = Integer.parseInt(InputDataHandler.getValue("periode"));
-            if (i % 12 == 0) {
+             int periode = Integer.parseInt(InputDataHandler.getValue("periode"));
+            if (i % periode == 0) {
                 //ca.print(i, null);
                 ca.calculatePoint(POAm, POAf, POEm, POEf, POLm, POLf, POIm, POIf, PCAm, PCAf, PCEm, PCEf, PCLm, PCLf, PCIm, PCIf, RMAm, RMAf, RMIm, RMIf);
-               // System.out.println(ca.calculatePoint(POAm, POAf, POEm, POEf, POLm, POLf, POIm, POIf, PCAm, PCAf, PCEm, PCEf, PCLm, PCLf, PCIm, PCIf, RMAm, RMAf, RMIm, RMIf));
+                // System.out.println(ca.calculatePoint(POAm, POAf, POEm, POEf, POLm, POLf, POIm, POIf, PCAm, PCAf, PCEm, PCEf, PCLm, PCLf, PCIm, PCIf, RMAm, RMAf, RMIm, RMIf));
                 Entrepreneurs[] nE;
-                nE = ca.stateTransition(ca,composition);
+                nE = ca.stateTransition(ca, composition);
                 for (int j = 0; j < nE.length; j++) {
                     System.out.println(nE[j].toString2());
                 }
